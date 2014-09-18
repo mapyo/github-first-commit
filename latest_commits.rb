9955ded669f5e0e9b8f5a6e9722d0f4282462ae7
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-09-18 22:49:03 +0900

    gheに対応した

--- latest_commits.rb
+++ latest_commits.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
-require 'uri'
 require 'json'
+require 'open-uri'
 
-require 'pry'
 
 repo_path = ENV['REPO_PATH'] # ex. mapyo/helloworld/
 github_api_url = (ENV['GITHUB_API_URL'] || 'https://api.github.com/') + 'repos/'
 github_url = (ENV['GITHUB_URL'] || 'https://github.com/')
+token = ENV['TOKEN']
 
 commits_path = 'commits/master'
 
@@ -21,28 +20,34 @@ end
 
 api_url = github_api_url + repo_path + commits_path
 
-uri = URI.parse(api_url)
-json = Net::HTTP.get(uri)
-result = JSON.parse(json)
 
+
+if token.nil?
+  res = open(api_url).read
+else
+  res = open(api_url, 'Authorization' => "token #{token}").read
+end
+
+
+result = JSON.parse(res)
 
 result_commit = result['commit']
 
-name = result_commit['committer']['name']
-message = result_commit['message']
 
-/#\d{1}/ =~ message
-/\d{1}/ =~ $&
-pull_number = $&
+name = result_commit['committer']['name']
+
+message = result_commit['message']
+match_string = message.match(/#(\d+)/)
+pull_number = match_string[1]
 
 
 # Whether the merge log pull request
-if (/^[0-9]+$/ =~ pull_number)
+if pull_number.nil?
+  commit_message = message
+else
   pull_url = github_url + repo_path + 'pull/' + pull_number
   pull_title = message.slice(message.index("\n\n") + 2 ... message.length)
   commit_message = pull_title + " " + pull_url
-else
-  commit_message = message
 end
 
 
